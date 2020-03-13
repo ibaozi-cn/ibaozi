9d10ccd2ab562a14183857e4fe7f8c614744760e
zzy <zhangzhanyong@julive.com>
2020-03-13 20:12:35 +0800

    【build web】

--- build-web.sh
+++ build-web.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-flutter build web
-
-rm build/web web.zip
-
-zip build/web web.zip
-
-cp build/web.zip web.zip
+#flutter build web
+#zip build/web web.zip
+#cp build/web.zip web.zip
+#rm build/web.zip
 
 git add .
 git commit -m "【build web】"
