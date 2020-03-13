75c3131a667522a0b2109b1163e84437ba0d8762
zzy <zhangzhanyong@julive.com>
2020-03-13 20:20:23 +0800

    【build web】

--- lib/about/assets.dart
+++ lib/about/assets.dart
@@ -4,7 +4,7 @@ class Assets {
   static const String programmer3 = 'images/programer.gif';
 
   // social icons
-  static const String qq = 'images/qq.png';
+  static const String qq = 'images/tx.png';
   static const String jianshu = 'images/jianshu.png';
   static const String gmail = 'images/gmail.png';
   static const String weixin = 'images/weixin.png';
