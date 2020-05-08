525cc3bc6d6de34fb46a36f098b6fe7fdf6e4f69
zzy <zhangzhanyong@julive.com>
2020-05-08 18:25:12 +0800

    【关闭Html】

--- node/server/app.js
+++ node/server/app.js
@@ -12,7 +12,7 @@ var app = express();
 // view engine setup
 //app.set('views', path.join(__dirname, 'views'));
 //app.set('view engine', 'pug');
-app.set('view engine', 'html');
+//app.set('view engine', 'html');
 
 app.use(logger('dev'));
 app.use(express.json());
