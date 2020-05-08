ccccd9236b13a3729ad4d7de180a1dccb6e30595
zzy <zhangzhanyong@julive.com>
2020-05-08 18:22:30 +0800

    【设置网页Html】

--- node/server/app.js
+++ node/server/app.js
@@ -12,6 +12,7 @@ var app = express();
 // view engine setup
 //app.set('views', path.join(__dirname, 'views'));
 //app.set('view engine', 'pug');
+app.set('view engine', 'html');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public_flutter_web')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+//app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
